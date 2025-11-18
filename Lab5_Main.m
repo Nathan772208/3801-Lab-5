251426afbeb26d1e6429b3c94ae71b28a9231a9c
Chris-Hat <christian.hattingh@yahoo.com>
2025-11-18 10:21:00 -0700

    Working

--- Lab5_Main.m
+++ Lab5_Main.m
@@ -5,19 +5,7 @@
 
 %% Housekeeping
 clear; clc; close all;
-<<<<<<< Updated upstream
 ttwistor;
-
-%% Question Toggles
-Q2_1 = 1;
-Q2_2 = 1;
-Q2_3 = 1;
-Q3_1 = 0;
-Q3_2 = 0;
-
-%% Q2
-=======
-ttwistor
 
 %% Question Toggles
 Q2_1 = 0;
@@ -67,20 +55,7 @@ x0_23 = [0;
          deg2rad(-0.2);
          0];       % angular velocity (deg/s)^
 
->>>>>>> Stashed changes
 if Q2_1 == 1
-    x0_21 = [0;
-        0;
-        -1609.34; % position ^
-        0;
-        0;
-        0;       % orientation ^
-        21;
-        0;
-        0;       % velocity ^
-        0;
-        0;
-        0];      % angular velocity ^
 
     % wind in the inertial frame
     wind_inertial_21 = [0; 0; 0]; % Define wind vector in the inertial frame
@@ -102,31 +77,13 @@ if Q2_1 == 1
 end
 
 if Q2_2 == 1
-    x0_22 = [0;
-        0;
-        -1800; % position ^
-        0;
-        0.02780;
-        0;       % orientation ^
-        20.99;
-        0;
-        0.5837;       % velocity ^
-        0;
-        0;
-        0];      % angular velocity ^
-
     % wind in the inertial frame
-<<<<<<< Updated upstream
     wind_inertial_22 = [0; 0; 0]; % Define wind vector in the inertial frame
     
     % control inputs vector
     aircraft_surfaces_22 = [0.1079; 0; 0; 0.3182]; % Define control inputs vector
-=======
-    % wind_inertial = [0; 0; 0]; % Define wind vector in the inertial frame
 
-    % control inputs vector
-    % aircraft_surfaces = [0.1079; 0; 0; 0.3182]; % Define control inputs vector
->>>>>>> Stashed changes
+    wind_inertial = [0; 0; 0]; % Define wind vector in the inertial frame
 
     [t_part22, x_part22] = ode45(@(t,x) AircraftEOM(t,x,aircraft_surfaces_22,wind_inertial_22,aircraft_parameters), [0 200], x0_22);
 
@@ -141,28 +98,6 @@ if Q2_2 == 1
 end
 
 if Q2_3 == 1
-<<<<<<< Updated upstream
-=======
-    % wind in the inertial frame
-    % wind_inertial = [0; 0; 0]; % Define wind vector in the inertial frame
-
-    % control inputs vector
-    % aircraft_surfaces = [0; 0; 0; 0]; % Define control inputs vector
->>>>>>> Stashed changes
-    
-    x0_23 = [0;
-        0;
-        -1800; % position ^
-        deg2rad(15);
-        deg2rad(-12);
-        deg2rad(270);       % orientation ^
-        19;
-        3;
-        -2;       % velocity ^
-        deg2rad(0.08);
-        deg2rad(-0.2);
-        0];      % angular velocity ^
-
 
     % wind in the inertial frame
     wind_inertial_23 = [0; 0; 0]; % Define wind vector in the inertial frame
@@ -183,13 +118,11 @@ if Q2_3 == 1
 end
 
 %% Q3
-if Q3_1 == 1
-<<<<<<< Updated upstream
-=======
-    % Initial Values
+% Initial Values
     doublet_size = deg2rad(15); % (deg)
     doublet_time = 0.25; % (sec)
 
+if Q3_1 == 1
     % wind in the inertial frame
     wind_inertial = [0; 0; 0]; % Define wind vector in the inertial frame
 
@@ -201,17 +134,10 @@ if Q3_1 == 1
                                    doublet_time, wind_inertial, aircraft_parameters), [0.5 1.5], x0_22);
     x_part31 = x_part31'; % make x_part21 a column vector
     PlotAircraftSim(t_part31, x_part31, aircraft_surfaces, [1, 2, 3, 4, 5, 6], 'r'); % Generate Plots
->>>>>>> Stashed changes
-
 end
 
 if Q3_2 == 1
-<<<<<<< Updated upstream
-=======
-    % Initial Values
-    doublet_size = deg2rad(15); % (deg)
-    doublet_time = 0.25; % (sec)
-
+  
     % wind in the inertial frame
     wind_inertial = [0; 0; 0]; % Define wind vector in the inertial frame
 
@@ -223,6 +149,5 @@ if Q3_2 == 1
                                    doublet_time, wind_inertial, aircraft_parameters), [1.5 100], x0_22);
     x_part31 = x_part31'; % make x_part21 a column vector
     PlotAircraftSim(t_part31, x_part31, aircraft_surfaces, [7, 8, 9, 10, 11, 12], 'r'); % Generate Plots
->>>>>>> Stashed changes
 
 end
